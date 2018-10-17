af0fbbc42dd9c9595ffe1a5fb286788ba22463fc
keeganthomp <whereiskeegan@gmail.com>
2018-10-17 11:57:24 -0500

    trying another thing 123212

--- server/server.js
+++ server/server.js
@@ -27,6 +27,10 @@ app.post('/api/artist/login', db.getArtistLogin)
 app.get('/api/artist/:id', db.getArtist)
 app.patch('/api/artist/:id', db.fileUpload)
 app.patch('/api/update/art/:artistId', db.updateArt)
+app.use( express.static( `${__dirname}/../build` ) )
+// app.get('*', (req, res)=>{
+//   res.sendFile(path.join(__dirname, '../build/index.html'));
+// })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
 module.exports = router

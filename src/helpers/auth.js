66c72b053ef4233645e8706c4daaeeec0ab03b11
keeganthomp <whereiskeegan@gmail.com>
2018-12-09 17:56:51 -0600

    another hotfix

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,6 +1,6 @@
 // import { verifyUser } from '../api'
 
-export const checkForValidUser = (callbackOnSuccess, callbackOnFailure) => {
+export const checkForValidUser = () => {
   // const isTokenExist = Boolean(sessionStorage.getItem('token'))
   // const token = isTokenExist && JSON.parse(sessionStorage.getItem('token'))
   // verifyUser(token, callbackOnSuccess, callbackOnFailure)

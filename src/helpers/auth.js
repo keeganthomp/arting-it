0e2a7a6ffd4db102587eaf816330ff9c5f1341f4
keeganthomp <whereiskeegan@gmail.com>
2018-12-09 18:07:01 -0600

    ....yah

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -2,7 +2,7 @@ import { verifyUser } from '../api'
 
 export const checkForValidUser = (callbackOnSuccess, callbackOnFailure) => {
   const isTokenExist = Boolean(sessionStorage.getItem('token'))
-  console.log('IS TOKEN EXIST::', isTokenExist)
+  console.log('IS TOKEN EXIST::', sessionStorage.getItem('token'))
   const token = isTokenExist && JSON.parse(sessionStorage.getItem('token'))
   console.log('TOKEN::', token)
   verifyUser(token, callbackOnSuccess, callbackOnFailure)

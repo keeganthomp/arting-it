2b5ec0adbd378c0b2aa592096e80302d9ea92bc5
keeganthomp <whereiskeegan@gmail.com>
2018-12-09 18:02:38 -0600

    addgin check back in

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,7 +1,9 @@
-// import { verifyUser } from '../api'
+import { verifyUser } from '../api'
 
-export const checkForValidUser = () => {
-  // const isTokenExist = Boolean(sessionStorage.getItem('token'))
-  // const token = isTokenExist && JSON.parse(sessionStorage.getItem('token'))
-  // verifyUser(token, callbackOnSuccess, callbackOnFailure)
+export const checkForValidUser = (callbackOnSuccess, callbackOnFailure) => {
+  const isTokenExist = Boolean(sessionStorage.getItem('token'))
+  console.log('IS TOKEN EXIST::', isTokenExist)
+  const token = isTokenExist && JSON.parse(sessionStorage.getItem('token'))
+  console.log('TOKEN::', token)
+  verifyUser(token, callbackOnSuccess, callbackOnFailure)
 }

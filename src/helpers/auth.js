79b4bd1571d9df090650e3fdacc6abc4244df2c4
keeganthomp <whereiskeegan@gmail.com>
2018-12-09 18:10:44 -0600

    fixing undefined session var

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,9 +1,7 @@
 import { verifyUser } from '../api'
 
 export const checkForValidUser = (callbackOnSuccess, callbackOnFailure) => {
-  const isTokenExist = Boolean(sessionStorage.getItem('token'))
-  console.log('IS TOKEN EXIST::', sessionStorage.getItem('token'))
+  const isTokenExist = sessionStorage.getItem('token') !== 'undefined'
   const token = isTokenExist && JSON.parse(sessionStorage.getItem('token'))
-  console.log('TOKEN::', token)
   verifyUser(token, callbackOnSuccess, callbackOnFailure)
 }

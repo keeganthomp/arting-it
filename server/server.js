2c90cec4f78186aeb105df6530a7436c283a1609
keeganthomp <whereiskeegan@gmail.com>
2019-01-15 21:00:53 -0600

    adding in .file thing

--- server/server.js
+++ server/server.js
@@ -57,6 +57,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(express.static(__dirname + '/public'))
 app.use( express.static( `${__dirname}/../build` ))
+app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 
 // app.get('/*', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../build/index.html'), (err) => {

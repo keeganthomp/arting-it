ff1b03c73786f2700cb9e9791c3e7d42a8c086a9
keeganthomp <whereiskeegan@gmail.com>
2019-05-21 14:12:47 -0500

    wrapping app in JSS provider

--- src/index.js
+++ src/index.js
@@ -7,13 +7,21 @@ import registerServiceWorker from './registerServiceWorker'
 import { Provider } from 'react-redux'
 import configureStore from './store/store'
 import { PersistGate } from 'redux-persist/integration/react'
+import JssProvider from 'react-jss/lib/JssProvider'
+import { create } from 'jss'
+import { createGenerateClassName, jssPreset } from '@material-ui/core/styles'
 
 const { store, persistor } = configureStore()
 
+const generateClassName = createGenerateClassName()
+const jss = create(jssPreset())
+
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}> 
-      <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <JssProvider jss={jss} generateClassName={generateClassName}>
+        <App />
+      </JssProvider>
     </PersistGate>
   </Provider>, document.getElementById('root'))
 registerServiceWorker()
